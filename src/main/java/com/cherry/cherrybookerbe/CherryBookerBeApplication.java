d333e55393755d507ff85369b4ba69613839c296
penrose0214 <penrose0214@gmail.com>
2025-12-05 19:30:44 +0900

    init: created basic Spring project

---
+++ src/main/java/com/cherry/cherrybookerbe/CherryBookerBeApplication.java
@@ -0,0 +1,13 @@
+package com.cherry.cherrybookerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherryBookerBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CherryBookerBeApplication.class, args);
+    }
+
+}
