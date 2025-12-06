7a255804d8f5f9a23601ccd7758c9b63315ccc3f
yeonsoo89 <dustn9689@gmail.com>
2025-12-06 13:35:54 +0900

    refact : 네비게이션바 active 추가 및 로고 폰트 변경

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,11 +1,17 @@
 <template>
+  <link href="https://fonts.googleapis.com/css2?family=Angkor&display=swap" rel="stylesheet">
+
   <nav class="nav-container">
     <!-- 왼쪽 영역 -->
-    <div class="nav-item" @click="goTo('/mypage')">
+    <div class="nav-item"
+         :class="{ active: isActive('/mypage') }"
+         @click="goTo('/mypage')">
       <img src="/images/mypage.png" alt="user" />
     </div>
 
-    <div class="nav-item" @click="goTo('/books')">
+    <div class="nav-item"
+         :class="{ active: isActive('/books') }"
+         @click="goTo('/books')">
       <img src="/images/book.png" alt="book" />
     </div>
 
@@ -15,23 +21,31 @@
     </div>
 
     <!-- 오른쪽 영역 -->
-    <div class="nav-item" @click="goTo('/quotes')">
+    <div class="nav-item"
+         :class="{ active: isActive('/quotes') }"
+         @click="goTo('/quotes')">
       <img src="/images/quote.png" alt="quote" />
     </div>
 
-    <div class="nav-item" @click="goTo('/menu')">
+    <div class="nav-item"
+         :class="{ active: isActive('/menu') }"
+         @click="goTo('/menu')">
       <img src="/images/thread.png" alt="menu" />
     </div>
   </nav>
 </template>
 
 <script setup>
-import { useRouter } from "vue-router";
+import { useRouter, useRoute } from "vue-router";
+
 const router = useRouter();
+const route = useRoute();
 
 const goTo = (path) => {
   router.push(path);
 };
+
+const isActive = (path) => route.path.startsWith(path);
 </script>
 
 <style scoped>
@@ -43,6 +57,23 @@ const goTo = (path) => {
   align-items: center;
   border-bottom: 1px solid #f0f0f0;
   background: #ffffff;
+  position: relative;
+}
+
+.nav-item {
+  position: relative;
+}
+
+.nav-item.active::after {
+  content: "";
+  position: absolute;
+  bottom: -13px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 700%;
+  height: 4px;
+  background-color: #EF3E42;
+  border-radius: 2px;
 }
 
 .nav-item img {
@@ -61,8 +92,8 @@ const goTo = (path) => {
 .nav-logo {
   font-size: 20px;
   font-weight: 700;
-  color: #df3e3e; /* 체리색 */
+  color: #EF3E42; /* 체리색 */
   cursor: pointer;
-  font-family: "Pretendard", sans-serif;
+  font-family: "Angkor", serif;
 }
 </style>
