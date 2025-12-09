6c024682f9208425281a465fc89debc023c997d4
longlive13 <papiairplane@gmail.com>
2025-12-09 11:59:30 +0900

    feat : 관리자 신고 처리 대상 조회 쿼리 추가
    - PENDING 상태이면서 신고 5회 이상인 게시글 ID 조회 쿼리 추가
    - PENDING 상태이면서 신고 5회 이상인 댓글 ID 조회 쿼리 추가
    - 관리자 신고 처리 대기 목록 조회를 위한 QueryRepository 구성

--- src/main/java/com/cherry/cherrybookerbe/report/query/ReportQueryRepository.java
+++ src/main/java/com/cherry/cherrybookerbe/report/query/ReportQueryRepository.java
@@ -1,7 +1,31 @@
 package com.cherry.cherrybookerbe.report.query;
 
 import com.cherry.cherrybookerbe.report.domain.Report;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 // 5회 이상 신고 받은 글 목록(pending상태) 조회
 public interface ReportQueryRepository extends JpaRepository<Report, Long> {
+
+    @Query("""
+            SELECT r.threads.id
+            FROM Report r
+            WHERE r.status = 'PENDING'
+            AND r.threads IS NOT NULL
+            GROUP BY r.threads.id
+            HAVING COUNT(r.reportId) >=5
+            """)
+    List<Long> findPendingThreadIdsReportedOverFive();
+
+    @Query("""
+        SELECT r.threadsReply.id
+        FROM Report r
+        WHERE r.status = 'PENDING'
+        AND r.threadsReply IS NOT NULL
+        GROUP BY r.threadsReply.id
+        HAVING COUNT(r.reportId) >= 5
+    """)
+    List<Long> findPendingReplyIdsReportedOverFive();
+
 }
